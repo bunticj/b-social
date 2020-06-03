438dcd50c404bea5e7dd198df9d8081a7da54e1b
jdsf dfs <bunticj11@gmail.com>
2020-06-03 18:58:06 +0100

    kafka producer on user registration

--- api/kafka-node/producer.js
+++ api/kafka-node/producer.js
@@ -5,8 +5,7 @@ function produceMessage(payloads){
 const HighLevelProducer = kafka.HighLevelProducer,
 client = new kafka.KafkaClient({kafkaHost: '192.168.99.100:9092'}),
 producer = new HighLevelProducer(client);
-
-    
+   
         producer.on('ready', function () {
             producer.send(payloads, function (err, data) {
                 console.log(payloads);
