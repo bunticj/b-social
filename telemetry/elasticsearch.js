5608c2cf1cb61f6819ade80fde528c1ca6ec15ce
Josip Buntic <bunticj11@gmail.com>
2020-06-09 05:05:49 +0100

    add elastic queries file

--- telemetry/elasticsearch.js
+++ telemetry/elasticsearch.js
@@ -12,15 +12,3 @@ module.exports.run = async function run (bodyObj,indexName) {
     console.log('Kafka message written to Elasticsearch successfully. Index name : ',indexName);
 }
 
-/*
-    let   {body}= await client.search({
-        index: 'posts',
-        body: {
-          query: {
-            match: { post_id: '1' }
-          }
-        }
-      })
-     
-      console.log(body.hits.hits)
-*/
